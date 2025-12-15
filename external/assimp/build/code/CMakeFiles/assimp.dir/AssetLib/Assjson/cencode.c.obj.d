95a0ad3bb5080bcd413cfe59e8df275e37b20e78
Zan3y <caleb.zane.webster@gmail.com>
2025-12-14 19:27:38 -0800

    Rebuilt assimp

---
+++ external/assimp/build/code/CMakeFiles/assimp.dir/AssetLib/Assjson/cencode.c.obj.d
@@ -0,0 +1,3 @@
+code/CMakeFiles/assimp.dir/AssetLib/Assjson/cencode.c.obj: \
+ C:\Users\TLZGa\Desktop\CodingProjects\C++\WebsterEngine\external\assimp\code\AssetLib\Assjson\cencode.c \
+ C:\Users\TLZGa\Desktop\CodingProjects\C++\WebsterEngine\external\assimp\code\AssetLib\Assjson\cencode.h
---
+++ external/assimp/build/code/CMakeFiles/assimp.dir/__/contrib/Open3DGC/o3dgcTools.cpp.obj.d
@@ -0,0 +1,2 @@
+code/CMakeFiles/assimp.dir/__/contrib/Open3DGC/o3dgcTools.cpp.obj: \
+ C:\Users\TLZGa\Desktop\CodingProjects\C++\WebsterEngine\external\assimp\contrib\Open3DGC\o3dgcTools.cpp

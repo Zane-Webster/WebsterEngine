f90c12893871470c6292685b22abd2bb75bba81c
Zan3y <caleb.zane.webster@gmail.com>
2026-02-14 13:40:17 -0800

    Updated version numbers and updated splashscreen

--- include/cfg/cfg.h
+++ include/cfg/cfg.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define WE_NAME "WebsterEngine"
-#define WE_VERSION_MAJOR 0
-#define WE_VERSION_MINOR 1
+#define WE_VERSION_MAJOR 1
+#define WE_VERSION_MINOR 0
 #define WE_VERSION_PATCH 0
 
 #define WE_PLATFORM_WINDOWS 1
